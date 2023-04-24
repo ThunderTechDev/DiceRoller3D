8906270db5f2cc9a22967e8f8fd04263423c8c57
Sergio González <thundertechdev@icloud.com>
2023-04-24 22:32:18 +0200

    Initial Commit

---
+++ DiceRoller3D/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiceRoller3D
+//
+//  Created by Sergio Gonzalez Cristobal on 24/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceRoller3D/DiceRoller3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRoller3DApp.swift
+//  DiceRoller3D
+//
+//  Created by Sergio Gonzalez Cristobal on 24/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRoller3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
