6106183c757477315c364e2852ef92118a98274d
Sergio González <thundertechdev@icloud.com>
2023-05-11 00:11:20 +0200

    Adding Launchscreen Formatting Button

--- DiceRoller3D/Views/ContentView.swift
+++ DiceRoller3D/Views/ContentView.swift
@@ -14,34 +14,58 @@ struct ContentView: View {
     @State private var sceneKitView: SCNView?
     
     var body: some View {
-        VStack {
-            Text("DiceRoller3D")
-                .font(.custom("Arial Rounded MT Bold", size: 50))
-                .fontWeight(.bold)
-                .foregroundColor(Color.cyan)
-            ZStack {
-                RoundedRectangle(cornerRadius: 14)
-                    .strokeBorder(Color.black, lineWidth: 20)
-                
-                SceneKitView(sceneKitView: $sceneKitView)
-                    .frame(width: 385, height: 385)
-                    .cornerRadius(9)
-            }
-            .frame(width: 400, height: 400)
-            .cornerRadius(14)
-            
-            Button("Lanzar dados", action: {
-                if let redDice = sceneKitView?.scene?.rootNode.childNode(withName: "RedDice", recursively: true),
-                   let whiteDice = sceneKitView?.scene?.rootNode.childNode(withName: "WhiteDice", recursively: true) {
-                    diceViewModel.applyRandomImpulseAndRotation(to: [redDice, whiteDice])
-                    diceViewModel.playAudios(sceneKitView: sceneKitView)
-                }
-            })
-            .frame(width: 100, height: 100)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]),
+                                       startPoint: .top,
+                                       endPoint: .bottom)
+                            .edgesIgnoringSafeArea(.all)
 
+        
+            VStack {
+                Text("DiceRoller3D")
+                    .font(.custom("Arial Rounded MT Bold", size: 50))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 14)
+                        .strokeBorder(Color.black, lineWidth: 20)
+                    
+                    SceneKitView(sceneKitView: $sceneKitView)
+                        .frame(width: 385, height: 385)
+                        .cornerRadius(9)
+                }
+                .frame(width: 400, height: 400)
+                .cornerRadius(14)
+                
+                .padding()
+                
+                Button(action: {
+                    if let redDice = sceneKitView?.scene?.rootNode.childNode(withName: "RedDice", recursively: true),
+                       let whiteDice = sceneKitView?.scene?.rootNode.childNode(withName: "WhiteDice", recursively: true) {
+                        diceViewModel.applyRandomImpulseAndRotation(to: [redDice, whiteDice])
+                        diceViewModel.playAudios(sceneKitView: sceneKitView)
+                    }
+                }) {
+                    Text("Lanzar Dados")
+                        .font(.custom("Arial Rounded MT Bold", size: 23))
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.white)
+                        .bold()
+                        .padding(EdgeInsets(top: 50, leading: 50, bottom: 50, trailing: 50))
+                        .background(Color(red: 8/255, green:155/255, blue: 80/255))
+                        .clipShape(Circle())
+                        .shadow(color: .black, radius: 30, x: 0, y: 0)
+                        .lineLimit(2)
+                        
+                }
+            }
+            
+            .padding()
         }
-        .padding()
-    }
+        }
+        
+        
+        
 }
 
 
